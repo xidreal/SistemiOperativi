6e44bd863408da7755d5c2f69015347f5298a28a
xentropy <xentropye@gmail.com>
2020-04-06 20:47:13 +0200

    ese4 done

--- L2_C_solutions/ese_4/src/main.c
+++ L2_C_solutions/ese_4/src/main.c
@@ -17,10 +17,14 @@ int main (int argc, char *argv[]) {
     int n = (int)(((double)rand() / RAND_MAX) * MAX_NUM);
     int m = (int)(((double)rand() / RAND_MAX) * MAX_NUM);
 
-    //....
+    char nStr [100];
+    char mStr [100];
+
+    sprintf(nStr, "%d", n);
+    sprintf(mStr, "%d", m);
 
     // call execl to run moltiplicatore
-    execl("moltiplicatore", /*...*/);
+    execl("moltiplicatore", "moltiplcatore", nStr, mStr, (char *) NULL);
     errExit("execlp failed");
 
     return 1;
--- L2_C_solutions/ese_4/src/moltiplicatore.c
+++ L2_C_solutions/ese_4/src/moltiplicatore.c
@@ -8,9 +8,11 @@ int main (int argc, char *argv[]) {
         return 1;
     }
 
-    // ...
+    int n = atoi(argv[1]);
+    int m = atoi(argv[2]);
 
-    printf("il prodotto di %d e %d e': %d\n", /*...*/;
-
+    printf("il prodotto di %d e %d e': %d\n", n, m, (n*m));
+    printf("il mio nome è : %s \n", argv[0]);
+    
     return 0;
 }
