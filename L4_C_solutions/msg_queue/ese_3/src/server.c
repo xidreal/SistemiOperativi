3e99d6108c63e96095beaeda90cb5afca9dbfa3a
xidreal <xentropye@gmail.com>
2020-04-22 09:39:01 +0000

    ese_3 done

--- L4_C_solutions/msg_queue/ese_3/src/server.c
+++ L4_C_solutions/msg_queue/ese_3/src/server.c
@@ -14,8 +14,11 @@ int msqid = -1;
 
 void signTermHandler(int sig) {
     // do we have a valid message queue identifier?
-    if (msqid > 0) {
-        //...
+     if (msqid > 0) {
+      if(msgctl(msqid, IPC_RMID, NULL) == -1)
+        errExit("msgctl failed");
+    } else {
+      printf("<Server> message queue removed successfully\n");
     }
 
     // terminate the server process
@@ -39,14 +42,16 @@ int main (int argc, char *argv[]) {
     // set of signals (N.B. it is not initialized!)
     sigset_t mySet;
     // blocking all signals but SIGTERM
-    // ...
-
+    sigfillset(&mySet);
+    sigdelset(&mySet, SIGTERM);
+    sigprocmask(SIG_SETMASK, &mySet, NULL);
     // set the function sigHandler as handler for the signal SIGTERM
-    // ...
+    if(signal(SIGTERM, signTermHandler) == SIG_ERR)
+      errExit("change signal handler failed");
 
     printf("<Server> Making MSG queue...\n");
     // get the message queue, or create a new one if it does not exist
-    msqid = // ...
+    msqid = msgget(msgKey, IPC_CREAT | S_IRUSR | S_IWUSR);
 
     // check functionality
     printf("<Server> sleep...\n");
@@ -60,7 +65,16 @@ int main (int argc, char *argv[]) {
     while (1) {
         // read a message from the message queue.
         errno = 0;
-        // ...
+        size_t mSize = sizeof(struct order) - sizeof(order.mtype);
+        if(msgrcv(msqid, &order, mSize, -2, IPC_NOWAIT ) == -1){
+          if(errno =0 ENoMSG){
+            printf("<Server> no roder here! Please contact marketing section\n");
+            sleep(60);
+            continue; // non credo sia essenziale
+          } else {
+            errExit("msgset failed");
+          }
+        }
         
         // print the order on standard output
         printOrder(&order);
